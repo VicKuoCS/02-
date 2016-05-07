cc15a095ad061d9cfe57ecf7e7bd36af76c560f1
Vic <forsphone@gmai.com>
2016-05-07 10:02:40 +0800

    First commit

---
+++ 02 練習建立空白方案與方案資料夾/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_練習建立空白方案與方案資料夾
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
